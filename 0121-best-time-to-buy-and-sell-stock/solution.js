c7b0a155f1e7dc02a36461bc38fe55b597ecb6ad
polockprog2 <60640688+polockprog2@users.noreply.github.com>
2026-08-30 04:57:44 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 65.2 MB (49.74%)

---
+++ 0121-best-time-to-buy-and-sell-stock/solution.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    let minPrice = prices[0];
+    let maxProfit = 0;
+
+    for (let i = 1; i < prices.length; i++) {
+        minPrice = Math.min(minPrice, prices[i]);
+
+        let profit = prices[i] - minPrice;
+
+        maxProfit = Math.max(maxProfit, profit);
+    }
+
+    return maxProfit;
+};
