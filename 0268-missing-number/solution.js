fed4a049bea2717ccd4e41b5a5cb6b63acd73203
polockprog2 <60640688+polockprog2@users.noreply.github.com>
2026-08-30 05:33:49 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 57 MB (43.60%)

---
+++ 0268-missing-number/solution.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var missingNumber = function(nums) {
+    let result = 0;
+
+    for(let i = 0; i < nums.length; i++){
+        result ^= i ^ nums[i];
+    }
+    return result ^ nums.length;
+
+};
