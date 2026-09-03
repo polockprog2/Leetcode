88faf28a95cae40655513e3e2e331f20ce2d4c0b
polockprog2 <60640688+polockprog2@users.noreply.github.com>
2026-09-03 16:46:33 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 54.2 MB (36.52%)

---
+++ 1626-can-make-arithmetic-progression-from-sequence/solution.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var canMakeArithmeticProgression = function (arr) {
+    arr.sort((a, b) => a - b)
+
+let difference = arr[1] - arr[0]
+
+for (let i = 2; i < arr.length; i++) {
+    if (arr[i] - arr[i - 1] !== difference) {
+        return false
+    }
+}
+return true
+};
